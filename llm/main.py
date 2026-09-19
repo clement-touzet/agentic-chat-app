b9440a49842cbea8685c5eb93936397fcefee922
Clément Touzet <clement.touzet40@gmail.com>
2026-09-19 13:03:17 +0200

    added cors middleware orgins

--- llm/main.py
+++ llm/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from ollama import AsyncClient
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 client = AsyncClient()
 
 class ChatRequest(BaseModel):
